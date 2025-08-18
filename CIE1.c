03d25b5cb7a43befb48207316f409351d6f7d3a0
Vijetha-R23 <vijetharpadmashali@gmail.com>
2025-08-18 10:36:32 +0530

    Add files via upload

---
+++ CIE1.c
@@ -0,0 +1,15 @@
+//write a open mp program which launches a parallel region and it has each thread print its ID and team size
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+   
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();     
+        int num_threads = omp_get_num_threads();  
+        printf("Hello from thread %d out of %d threads\n", thread_id, num_threads);
+    }
+
+    return 0;
+}
